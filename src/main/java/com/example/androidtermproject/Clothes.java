803093f0de081817aa14b88103978dd0fb6e57e4
yy5538012 <yy5538012@naver.com>
2020-06-22 18:15:01 +0900

    Uploading information of clothes
    Uploading information of clothes that the user wants to register

---
+++ src/main/java/com/example/androidtermproject/Clothes.java
@@ -0,0 +1,30 @@
+package com.example.androidtermproject;
+
+public class Clothes {
+
+    public String user_id;
+    public String name_str;
+    public String category_str;
+    public String detail_str;
+    public String color_str;
+    public String style_str;
+
+    public Clothes()
+    {
+
+    }
+
+    public Clothes(String user_id, String name_str, String category_str, String detail_str, String color_str, String style_str)
+    {
+        this.user_id = user_id;
+        this.name_str = name_str;
+        this.category_str = category_str;
+        this.detail_str = detail_str;
+        this.color_str = color_str;
+        this.style_str = style_str;
+    }
+
+
+}
+
+
